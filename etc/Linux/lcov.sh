b879b45f9b68a701347103f2b9075a2f7bcd4719
Darby Johnston <darbyjohnston@yahoo.com>
2024-04-28 15:24:35 -0700

    Add filters

--- etc/Linux/lcov.sh
+++ etc/Linux/lcov.sh
@@ -2,6 +2,8 @@
 
 lcov -c -b . -d . -o coverage.info
 lcov -r coverage.info '*/usr/*' -o coverage_filtered.info
+lcov -r coverage_filtered.info '*/Python/*' -o coverage_filtered.info
+lcov -r coverage_filtered.info '*/examples/*' -o coverage_filtered.info
 lcov -r coverage_filtered.info '*/install/*' -o coverage_filtered.info
 lcov -r coverage_filtered.info '*/tests/*' -o coverage_filtered.info
 lcov -r coverage_filtered.info '*/deps/*' -o coverage_filtered.info
