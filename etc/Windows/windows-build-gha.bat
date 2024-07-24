1013c08251ee9bb9a8cdea7ff876713977fe5347
Darby Johnston <darbyjohnston@yahoo.com>
2024-07-24 16:48:22 -0700

    Build fix

--- etc/Windows/windows-build-gha.bat
+++ etc/Windows/windows-build-gha.bat
@@ -4,13 +4,13 @@ git clone https://github.com/darbyjohnston/dtk.git
 cmake -S dtk\etc\SuperBuild ^
   -B dtk\build ^
   -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
-  -DCMAKE_INSTALL_PREFIX=install ^
-  -DCMAKE_PREFIX_PATH=install
+  -DCMAKE_INSTALL_PREFIX=%CD%\install ^
+  -DCMAKE_PREFIX_PATH=%CD%\install
 cmake --build dtk\build -j 4 --config %BUILD_TYPE%
 
 cmake -S tinygfx ^
   -B tinygfx\build ^
   -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
-  -DCMAKE_PREFIX_PATH=install
+  -DCMAKE_PREFIX_PATH=%CD%\install
 cmake --build tinygfx\build -j 4 --config %BUILD_TYPE%
 
